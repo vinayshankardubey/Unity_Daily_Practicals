598f4d0351ba6d96a60c62e2fc3dc82f9fa44389
VinayDevs00 <vinaydevs00@gmail.com>
2021-09-13 09:10:06 +0530

    Practical 8 Done
    Practical 8 Done

--- Unity Daily Practicals/Assets/Scripts/Practical8.cs
+++ Unity Daily Practicals/Assets/Scripts/Practical8.cs
@@ -7,7 +7,16 @@ public class Practical8 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+
+        GameObject[] gO = new GameObject[10];
         
+        for(int i = 0 ; i < 10; i++){
+
+            gO[i] = GameObject.CreatePrimitive(PrimitiveType.Cube);
+
+            gO[i].transform.position = new Vector3(i + 1, i + 1, 0);
+
+        }
     }
 
     // Update is called once per frame
