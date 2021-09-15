eb6bb346547057e741d762a2fbbc707822241a41
VinayDevs00 <vinaydevs00@gmail.com>
2021-09-15 21:50:58 +0530

    Practical 8 Done
    Practical 8 Done

---
+++ Unity Daily Practicals/Assets/Practical81.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Practical81 : MonoBehaviour
+{
+    // Start is called before the first frame update
+            GameObject[] ap = new GameObject[10];
+
+
+    void Start()
+    {
+        
+
+        for( int i = 0; i <= 9; i++){
+            ap[i] = GameObject.CreatePrimitive(PrimitiveType.Cube);
+             
+             ap[i].transform.position = new Vector3(0 + i, 0 + i, 0);
+        }
+
+    
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        for(int i = 0; i <= 9; i++){
+
+            ap[i].GetComponent<Renderer>().material.color = Color.red;
+
+        }
+
+    }
+}
