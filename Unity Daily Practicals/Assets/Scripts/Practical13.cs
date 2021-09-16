ae81b3996a4215cf4a0da328406b9eaf029f4179
VinayDevs00 <vinaydevs00@gmail.com>
2021-09-16 09:48:52 +0530

    Practical 13 done
    Exposing a variable for bounciness

--- Unity Daily Practicals/Assets/Scripts/Practical13.cs
+++ Unity Daily Practicals/Assets/Scripts/Practical13.cs
@@ -2,16 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Practical12 : MonoBehaviour
+public class Practical13 : MonoBehaviour
 {
 
     public Color myColor = Color.red;
+    public float myBounciness = 0.5f;
     // Start is called before the first frame update
     void Start()
     {
 this.gameObject.AddComponent<Rigidbody>();
 PhysicMaterial material = new PhysicMaterial();
-material.bounciness = 1f;
+material.bounciness = myBounciness;
 this.GetComponent<Collider>().material = material;
 this.GetComponent<Renderer>().material.color = myColor;
 this.GetComponent<Renderer>().material.color = myColor;
