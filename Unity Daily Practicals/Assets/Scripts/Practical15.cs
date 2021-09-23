2a6880e28b4f94fa860e54fb87e14e89a2d9bbbb
VinayDevs00 <vinaydevs00@gmail.com>
2021-09-23 22:50:16 +0530

    Practical Day 15 Completed
    Today I Have Learn About Unity "Scripts as Behaviour Components"

---
+++ Unity Daily Practicals/Assets/Scripts/Practical15.cs
@@ -0,0 +1,23 @@
+
+
+using UnityEngine;
+using System.Collections;
+
+public class Practical15 : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            GetComponent<Renderer> ().material.color = Color.red;
+        }
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            GetComponent<Renderer>().material.color = Color.green;
+        }
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            GetComponent<Renderer>().material.color = Color.blue;
+        }
+    }
+}
